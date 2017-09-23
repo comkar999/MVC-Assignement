8823359059fce0992a71e41ba4b448bf8a9adfab
comkar999 <31873952+comkar999@users.noreply.github.com>
2017-09-23 18:43:33 -0500

    Add files via upload

---
+++ EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StronglyTypedViews;
+
+namespace StronglyTypedViews.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult EmployeeData(int id)
+        {
+            using (EmployeeDBEntities edb = new EmployeeDBEntities())
+            {
+                Employee emp = edb.Employees.SingleOrDefault(x => x.ID == id);
+
+                return View(emp);
+            }
+            
+               
+        }
+    }
+}
