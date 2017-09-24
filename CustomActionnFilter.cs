31a829f33522031f0d6acacad4467a87929ed828
comkar999 <31873952+comkar999@users.noreply.github.com>
2017-09-23 20:37:16 -0500

    Add files via upload

---
+++ CustomActionnFilter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace WebApplication9
+{
+    public class CustomActionnFilter : ActionFilterAttribute
+    {
+
+        public HttpRequest UsrAgnt
+        {
+            get
+            {
+                return HttpContext.Current.ApplicationInstance.Request;
+            }
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+
+           string strUsrAgnt= UsrAgnt.UserAgent+ DateTime.Now.ToString();
+            LogData(strUsrAgnt);
+
+           
+        }
+
+        public void LogData(string data )
+        {
+            File.AppendAllText(HttpContext.Current.Server.MapPath("~/Data/Data.txt"), data);
+        }
+
+
+
+    }
+}
